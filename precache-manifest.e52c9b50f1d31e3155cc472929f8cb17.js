6ec67cf9368fe74c28d53ea70d9bb400760e83b3
karyiteh <teh_karyi97@hotmail.com>
2020-06-24 11:33:57 -0700

    Updates

---
+++ precache-manifest.e52c9b50f1d31e3155cc472929f8cb17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43175b3204dca0a1bf08de1934c34d79",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "d13a6b457108853bd91c",
+    "url": "/tictactoe/static/css/main.b841dff3.chunk.css"
+  },
+  {
+    "revision": "dbdf6094b99a074ddd8a",
+    "url": "/tictactoe/static/js/2.094fd168.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.094fd168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13a6b457108853bd91c",
+    "url": "/tictactoe/static/js/main.3126eecb.chunk.js"
+  },
+  {
+    "revision": "8fd1009f88de2a8f7d1e",
+    "url": "/tictactoe/static/js/runtime-main.57baa19c.js"
+  }
+]);
